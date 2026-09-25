a5b02383a579fb2644dc9a4352cdc224f487a276
fel1x-Hub <miguel.20260210@institutojef.org.br>
2026-09-25 12:01:52 -0300

    Nova estrutura Spring Boot do projeto

---
+++ src/main/java/com/quimia/quimiawebcrud/QuimiaWebCrudApplication.java
@@ -0,0 +1,13 @@
+package com.quimia.quimiawebcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuimiaWebCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuimiaWebCrudApplication.class, args);
+    }
+
+}
